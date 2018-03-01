ac0951a3d615b38929957197ed67c8b2e2ac0cce
naviroyal <36540867+naviroyal@users.noreply.github.com>
2018-03-01 10:26:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,7 +11,6 @@ app.get('/', function (req, res) {
 
 app.get('/article-one',function(req,res) {
   res.sendFile(path.join(__dirname,'ui' ,'article-one.html')); 
-    
 });
 app.get('/article-two',function(req,res){
    res.send('Article two  requesting and will be served here'); 
