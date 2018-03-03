43726bda5eaa1fcc1f23e76cd12d37859f0bb1aa
naviroyal <36540867+naviroyal@users.noreply.github.com>
2018-03-03 21:04:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,7 @@
 console.log('Loaded!');
 var element= document.getElementById("mainText");
 element.innerHTML="hello navi";
+var img=document.getElementById("img");
+img.onclick=function(){
+  img.style.marginLeft="100px";  
+};
